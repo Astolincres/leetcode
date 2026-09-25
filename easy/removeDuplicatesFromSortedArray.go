f6e836168b6b10d03e40166e7a81767b364863a3
Astolfo Coronado <rugal_diego@hotmail.com>
2026-09-25 10:36:08 -0700

    adding removeDuplicatesFromSortedArray.go

---
+++ easy/removeDuplicatesFromSortedArray.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 8, 9}
+	fmt.Println(removeDuplicates(nums))
+}
+
+func removeDuplicates(nums []int) int {
+	return 0
+}
